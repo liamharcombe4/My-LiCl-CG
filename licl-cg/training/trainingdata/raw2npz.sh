a0cf5bddce27b5b531866ba6c96dbadcfafbf5c6
Liam Harcombe <liampharcombe@gmail.com>
2023-12-12 16:22:36 +1100

    no energies

--- licl-cg/training/trainingdata/raw2npz.sh
+++ licl-cg/training/trainingdata/raw2npz.sh
@@ -18,7 +18,6 @@ echo nline per set is $nline_per_set
 export nline=$nline_per_set
 
 split coord.raw	 -l $nline_per_set -d -a 3 coord.raw
-test -f energy.raw && split energy.raw -l $nline_per_set -d -a 3 energy.raw
 test -f force.raw  && split force.raw  -l $nline_per_set -d -a 3 force.raw
 test -f virial.raw && split virial.raw -l $nline_per_set -d -a 3 virial.raw
 test -f atom_ener.raw && split atom_ener.raw -l $nline_per_set -d -a 3 atom_ener.raw
@@ -35,7 +34,6 @@ do
   pi=`printf %03d $ii`
   mkdir set.$pi
   mv coord.raw$pi	set.$pi/coord.raw
-  test -f energy.raw$pi && mv energy.raw$pi set.$pi/energy.raw
   test -f force.raw$pi  && mv force.raw$pi  set.$pi/force.raw
   test -f virial.raw$pi && mv virial.raw$pi set.$pi/virial.raw
   test -f atom_ener.raw$pi && mv atom_ener.raw$pi set.$pi/atom_ener.raw
@@ -45,14 +43,6 @@ do
   if [ $ii -eq 0 ]
      then
   python3 -c 'import numpy as np; import os; data = np.loadtxt("coord.raw" , ndmin = 2); data = data.astype (np.float32); data = data.reshape(int(os.environ["nline"]),data.shape[1]//3,3); np.save ("R",  data)'
-  python3 -c \
-'import numpy as np; import os.path; 
-if os.path.isfile("energy.raw"): 
-   data = np.loadtxt("energy.raw"); 
-   data = data.astype (np.float32); 
-   data = data.reshape(int(os.environ["nline"]),1);
-   np.save ("E", data)
-'
   python3 -c \
 'import numpy as np; import os; 
 if os.path.isfile("force.raw" ): 
@@ -104,8 +94,8 @@ mv z.npy set.000
 cd set.000
 
 python3 -c '\
-import numpy as np; cell = np.load("cell.npy"); z = np.load("z.npy"); R = np.load("R.npy"); E = np.load("E.npy"); F = np.load("F.npy"); pbc = np.load("pbc.npy");
-np.savez("dataLiCl.npz", cell=cell, z=z, R=R, E=E, F=F, pbc=pbc)'
+import numpy as np; cell = np.load("cell.npy"); z = np.load("z.npy"); R = np.load("R.npy"); F = np.load("F.npy"); pbc = np.load("pbc.npy");
+np.savez("dataLiCl.npz", cell=cell, z=z, R=R, F=F, pbc=pbc)'
 
 mv dataLiCl.npz ..
 cd ..
